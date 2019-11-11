7e2b2e0fe7b36b580fa531578c6c6d7cca8e3ae8
Huỳnh Văn Hậu <huynhhau55@gmail.com>
2019-11-11 16:00:28 +0700

    Translate English to Vietnamese

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Hello");
+const express = require('express');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('./db/dictionary.json');
+const db = low(adapter);
+
+const app = express();
+
+app.get('/:en',(req,res) => {
+    const en = req.params.en;
+    const ret = db.get('words')
+        .find({word:{en}})
+        .value();
+    //console.log(ret);*/
+    res.json(ret);
+    
+})
+
+const port = 3000;
+app.listen(port,() =>{
+    console.log(`API running on port ${port}`);
+});
+
